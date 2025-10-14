ac7a87909fe4b687fe365f1c9e5d3758e4576aaa
Alker <gvelnor@gmail.com>
2025-10-15 02:28:14 +0700

    change: remove comment

--- electron/preload.js
+++ electron/preload.js
@@ -3,15 +3,6 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("electronAPI", {
   restart: () => ipcRenderer.invoke("app:restart"),
   reset: () => ipcRenderer.invoke("app:reset"),
-  // Traffic
-  // appTraffic: async (webContentsId) => {
-  //   await ipcRenderer.invoke("app:traffic", webContentsId);
-  // },
-  // onAppTrafficUpdate: (callback) => {
-  //   ipcRenderer.on("net:traffic", (event, data) => {
-  //     callback(data);
-  //   });
-  // },
 });
 
 contextBridge.exposeInMainWorld("network", {
